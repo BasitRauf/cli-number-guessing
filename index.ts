b43f0ac2bd641c5c553dc3b0ec81aeef793ae643
BasitRauf <ab1310200699@gmail.com>
2024-04-09 22:23:27 +0100

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/evn node
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+
+const answers = await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:"please Guess a number between 1-6:",
+    }
+]);
+
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulation! you guessed right number");
+    
+} else {
+    console.log("you guessed wrong number");
+};
